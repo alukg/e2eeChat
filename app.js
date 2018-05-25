58bbfa3771fd24df7e7aa26620d3ef40626ca84c
guy <guyaluk@gmail.com>
2018-05-25 20:25:30 +0300

    - Change csss location - Add comments

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
  * Created by guy on 22/03/18.
  */
 
+/**
+ * npm imports and definitions
+ */
 let dbFuncs = require('./dbFuncs.js'),
     express = require('express'),
     app = express(),
@@ -10,18 +13,27 @@ let dbFuncs = require('./dbFuncs.js'),
     session = require('express-session'),
     fs = require('fs');
 
+/**
+ * Defining base folder for project
+ */
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ * Defining server params
+ */
 const opts = { key: fs.readFileSync(__dirname + "/CA/server_key.pem", 'utf8')
     , cert: fs.readFileSync(__dirname + "/CA/server_cert.pem", 'utf8')
     , requestCert: true
-    , rejectUnauthorized: false
+    , rejectUnauthorized: false // In order to control it ourselves by messages
     , ca: [ fs.readFileSync(__dirname + "/CA/server_cert.pem", 'utf8') ] };
 
 let server = https.createServer(opts,app).listen(4200);
 
+/**
+ * Sessions management
+ */
 let sessStore = new session.MemoryStore();
 let sessionMiddleware = session({
     store: sessStore,
@@ -33,6 +45,9 @@ let sessionMiddleware = session({
 });
 app.use(sessionMiddleware);
 
+/**
+ * Page for authentication check - Displays the status of the user
+ */
 app.get('/authenticate', (req, res) => {
     cert = req.connection.getPeerCertificate();
     if (req.client.authorized) {
@@ -44,84 +59,126 @@ app.get('/authenticate', (req, res) => {
     }
 });
 
+/**
+ * Land page
+ */
 app.get("/", function (req, res) {
-    if(req.session.user)
+    if(req.session.user)    // If there is an active session
         res.redirect('/chat');
     else
         res.redirect('/login');
 });
+/**
+ * Login page request
+ */
 app.get("/login", function (req, res) {
     res.sendFile(__dirname + '/login.html');
 });
+/**
+ * Register page request
+ */
 app.get("/register", function (req, res) {
-    if (req.client.authorized) {
+    if (req.client.authorized) {    // Check if there is appropriate certificates before try to register
         res.sendFile(__dirname + '/register.html');
     } else {
         res.status(401).sendFile(__dirname + '/cert_err.html');
     }
 });
+/**
+ * Chat page request
+ */
 app.get("/chat", function (req, res) {
-    if(req.session.user) {
-        // check if user exists on db
+    if(req.session.user) {  // If there is an active session
+        // Check if user exists on db - Maybe the session still exists but the user not
         dbFuncs.checkIfUserExists(req.session.user).then(
             () => {
                 res.sendFile(__dirname + '/chat.html');
             }, (err) => {
+                // If not - close the session
                 req.session.destroy(function(err) {
                     if(err) {
                         console.log(err);
                         res.end();
                     } else {
+                        // Return to landing page
                         res.redirect('/');
                     }
                 });
             });
     }
-    else {
+    else {  // If there is no active session - redirect to login
         res.redirect('/login');
     }
 });
+/**
+ * User register request
+ */
 app.post('/register', function(req, res) {
-    let cert = req.connection.getPeerCertificate();
-    let certUsername = cert.subject["CN"];
-    if (certUsername !== req.body.email){
-        console.log("register error: wrong certificate");
-        res.redirect('/register');
-    }
-    else
-    {
-        dbFuncs.createUser(req.body.email,req.body.pass,cert).then(
+    let cert = req.connection.getPeerCertificate(); // get the certificate of the session to store them in the db
+    // let certUsername = cert.subject["CN"];
+    // if (certUsername !== req.body.email){
+    //     console.log("register error: wrong certificate");
+    //     res.redirect('/register');
+    // }
+    // else
+    // {
+        dbFuncs.createUser(req.body.email,req.body.pass,cert).then( // try to create user
             () => {
                 // sets a cookie with the user's info
                 req.session.user = req.body.email;
                 res.redirect('/chat');
             }, (err) => {
+                // if fail - redirect to register page with error to display
                 console.log("register error: " + err);
-                res.redirect('/register');
+                res.redirect('/register?err=' + err);
             });
-    }
+    // }
 });
+/**
+ * User login request
+ */
 app.post('/login', function(req, res) {
-    let cert = req.connection.getPeerCertificate();
-    let certUsername = cert.subject["CN"];
-    if (certUsername !== req.body.email){
-        console.log("signin error: wrong certificate");
-        res.redirect('/login');
-    }
-    else
-    {
-        dbFuncs.checkIfUserAndPassOk(req.body.email,req.body.pass).then(
-            () => {
-                // sets a cookie with the user's info
-                req.session.user = req.body.email;
-                res.redirect('/chat');
-            }, (err) => {
-                console.log("signin error: " + err);
-                res.redirect('/login');
-            });
-    }
+    // let cert = req.connection.getPeerCertificate();
+    // let certUsername = cert.subject["CN"];
+    // if (certUsername !== req.body.email){
+    //     console.log("signin error: wrong certificate");
+    //     res.redirect('/login');
+    // }
+    // else
+    // {
+        // check if the user already connected in the connected users array
+        let found = false;
+        for( let i=0; i < users.length; i++ ){
+            let c = users[i];
+            if(c["username"] === req.body.email){   // compare to the request username
+                found = true;
+                break;
+            }
+        }
+        if(found)   // if connected - redirect to login page with error to display
+        {
+            console.log("signin error: User already connected");
+            res.redirect('/login?err=User already connected');
+        }
+        else{   // if not connected - check if user exists on db
+            dbFuncs.checkIfUserAndPassOk(req.body.email,req.body.pass).then(
+                () => {
+                    // sets a cookie with the user's info
+                    req.session.user = req.body.email;
+                    res.redirect('/chat');
+                }, (err) => {
+                    // if not exists - redirect to login page with error to display
+                    console.log("signin error: " + err);
+                    res.redirect('/login?err=' + err);
+                });
+        }
+    // }
 });
+/**
+ * User logout request
+ */
 app.get('/logout', function(req, res) {
+    // destroy the session and redirect to login page
     req.session.destroy(function(err) {
         if(err) {
             console.log(err);
@@ -134,31 +191,47 @@ app.get('/logout', function(req, res) {
 
 // ---------------- Socket-io ----------------
 
-const sio = require('socket.io')(server);
+const sio = require('socket.io')(server);   // attach the socket.io process to the server
 
 let users = [];
 
+/**
+ * attach the socket.io process to the session management
+ * it means that if the session is closed the socket.io connection is terminated
+ */
 sio.use(function(socket, next) {
     sessionMiddleware(socket.request, socket.request.res, next);
 });
 
+/**
+ * Wait for new connection
+ */
 sio.on("connection", function(socket) {
     console.log('connect user: ' + socket.request.session.user);
     let sessionUser = socket.request.session.user;
 
-    users.push({socketId: socket.id, username: socket.request.session.user});
+    users.push({socketId: socket.id, username: socket.request.session.user});   // add the user to the connected user array
 
-    socket.broadcast.emit('connectUser', sessionUser);
+    socket.broadcast.emit('connectUser', sessionUser);  // broadcast all the users that there is new connected user
 
-    sendUsersList(sessionUser);
-    sendWaitingMessages(sessionUser);
+    sendUsersList(sessionUser); // send the list of all the users to the new user from the db
+    sendWaitingMessages(sessionUser);   // send all the waiting messages for the new user from the db
 
+    /**
+     * Every 2 minutes refresh the list of the users for that user
+     */
     setInterval(function(){
         sendUsersList(sessionUser);
     }, 2000);
 
+    /**
+     * Set message listener from that user to server
+     * These messages are intended for another users
+     * The server need to decide what to do with them
+     */
     socket.on('message', function (userdata) {
         let socketId = null;
+        // get the socket id of the user that the message are intended to
         for( let i=0; i < users.length; i++ ){
             let c = users[i];
             if(c["username"] === userdata["to"]){
@@ -166,14 +239,18 @@ sio.on("connection", function(socket) {
                 break;
             }
         }
-        if (socketId === null){
+        if (socketId === null){ // if there is no socket id, that means the user disconnected. In that case - store the message
             dbFuncs.pushMessageToServer(sessionUser,userdata["to"],userdata["data"]).then(() => {}, (err) => {console.log(err)});
         }
-        else{
+        else{   // if the user connected pass the message to that user through his socket
             sio.sockets.connected[socketId].emit('message',{from: sessionUser, data: userdata["data"]});
         }
     });
 
+    /**
+     * Get the users list from the server and pass to the socket user
+     * @param sessionUser - The name of the current user, to remove him from the list
+     */
     function sendUsersList(sessionUser){
         dbFuncs.getUsersList().then(
             (usersList) => {
@@ -186,6 +263,10 @@ sio.on("connection", function(socket) {
             });
     }
 
+    /**
+     * Get the waiting messages for some user
+     * @param sessionUser - The username of the user that the messages are intended to
+     */
     function sendWaitingMessages(sessionUser){
         dbFuncs.getWaitingMessages(sessionUser).then(
             (messages) => {
@@ -194,19 +275,24 @@ sio.on("connection", function(socket) {
             });
     }
 
+    /**
+     * Set a disconnect listener.
+     */
     socket.on('disconnect', function (data) {
         for( let i=0; i < users.length; i++ ){
             let c = users[i];
             if(c["socketId"] === socket.id){
-                users.splice(i,1);
-                socket.broadcast.emit('disconnectUser', sessionUser);
+                users.splice(i,1);  // remove the user from the connected users list
+                socket.broadcast.emit('disconnectUser', sessionUser);   // broadcast all the users that this user was disconnected
                 break;
             }
         }
     });
 });
 
+
 // ---------------- API ----------------
+// ajax get calls for files from the server
 
 app.get("/api/getCACert", function (req, res) {
     res.send(fs.readFileSync(__dirname + "/CA/server_cert.pem", 'utf8'));
@@ -222,6 +308,11 @@ app.get("/api/getUserCert", function (req, res) {
 
 // ---------------- Help functions ----------------
 
+/**
+ * Remove user by user name from list of names
+ * @param array - list of usernames
+ * @param element - name to remove
+ */
 function removeUserFromList(array, element) {
     let i;
     let found = 0;
@@ -231,10 +322,8 @@ function removeUserFromList(array, element) {
             break;
         }
     }
-
     if(i === array.length)
         i--;
-
     if(found === 1)
         array.splice(i,1);
 }
--- dbFuncs.js
+++ dbFuncs.js
@@ -2,44 +2,59 @@
  * Created by guy on 22/03/18.
  */
 
+/**
+ * npm imports and definitions
+ */
 const sqlite3 = require('sqlite3').verbose();
 const crypto = require('crypto');
 const forge = require('node-forge');
 const fs = require('fs');
 const pemtools = require('pemtools');
 
+/**
+ * Export modules for app.js use
+ * @type {{createUser: createUser, checkIfUserAndPassOk: checkIfUserAndPassOk, getUsersList: getUsersList, checkIfUserExists: checkIfUserExists, pushMessageToServer: pushMessageToServer, getWaitingMessages: getWaitingMessages, getUserCert: getUserCert}}
+ */
 module.exports = {createUser,checkIfUserAndPassOk,getUsersList,checkIfUserExists,pushMessageToServer,getWaitingMessages,getUserCert};
 
+/**
+ * Create new user and store in db all the params
+ * @param username
+ * @param pass
+ * @param cert - certificates of the user
+ * @returns Promise that creates a new user
+ */
 function createUser(username,pass,cert) {
     cert = pemtools(cert.raw, 'CERTIFICATE').toString(); // Peer certificate format to PEM format
-    cert = cert.replace(/\n/g,'');
+    cert = cert.replace(/\n/g,'');  // fix the format
 
     let query = (db) => {
+        let certString = JSON.stringify(cert);  // make a string from the cert for db storage as text
 
-        let certString = JSON.stringify(cert);
-
-        // create salt and hash password
+        /**
+         * create salt and hash password
+         */
         let salt =  Math.random().toString(36).substr(2);
         const hmac = crypto.createHmac('sha256', salt);
-
         hmac.update(pass);
         let hashedPass = hmac.digest('hex');
 
+        // create the needed queries for later invoke
         let getUserRow = `SELECT * FROM Clients WHERE username='${username}'`;
         let insertUser = `INSERT INTO Clients (username,pass,salt,cert) VALUES ('${username}','${hashedPass}','${salt}','${certString}')`;
 
         return new Promise((resolve,reject) => {
-            db.all(getUserRow, function(err, result){
+            db.all(getUserRow, function(err, result){   // check if there is a row with that username
                 if (err) {
                     closeDb(db);
                     return reject(err);
                 }
-                else if (result.length > 0) {
+                else if (result.length > 0) {   // if true
                     closeDb(db);
-                    return reject("Username exists");
+                    return reject("Username already exists");
                 }
                 else{
-                    db.all(insertUser, function(err, result){
+                    db.all(insertUser, function(err, result){   // try to insert user
                         closeDb(db);
                         if (err)
                             return reject(err);
@@ -53,26 +68,32 @@ function createUser(username,pass,cert) {
     return executeQuery(query);
 }
 
+/**
+ * compare the inserted user params to the db and check if they are fine
+ * @param username
+ * @param pass
+ * @returns Promise that makes the check
+ */
 function checkIfUserAndPassOk(username,pass) {
     let query = (db) => {
         let getUserRow = "SELECT * FROM Clients WHERE username='" + username + "'";
 
         return new Promise((resolve,reject) => {
-            db.all(getUserRow, function(err, result){
+            db.all(getUserRow, function(err, result){ // check if there is a row with that username
                 closeDb(db);
 
                 if (err)
                     return reject(err);
-                else if (result.length === 0)
+                else if (result.length === 0)   // if there is no line
                     return reject("User doesn't exists");
 
-                let salt =  result[0]['salt'];
+                // if there is user with that username
+                let salt =  result[0]['salt'];  // get the salt from the db
                 const hmac = crypto.createHmac('sha256', salt);
-
-                hmac.update(pass);
+                hmac.update(pass); // hash the inserted password with the salt from the db
                 let hashedPass = hmac.digest('hex');
 
-                if (hashedPass === result[0]['pass'])
+                if (hashedPass === result[0]['pass']) // if the current hash and the hash from the db are the same
                     return resolve();
                 else
                     return reject("Password incorrect");
@@ -83,6 +104,11 @@ function checkIfUserAndPassOk(username,pass) {
     return executeQuery(query);
 }
 
+/**
+ * checks if username exists in db
+ * @param username
+ * @returns Promise that checks if username exists in db
+ */
 function checkIfUserExists(username) {
     let query = (db) => {
         let getUserRow = "SELECT * FROM Clients WHERE username='" + username + "'";
@@ -104,6 +130,10 @@ function checkIfUserExists(username) {
     return executeQuery(query);
 }
 
+/**
+ * Get the list of all the users from the db
+ * @returns Promise that resolve the list of users
+ */
 function getUsersList(){
     let query = (db) => {
         let getUsers = "SELECT username,cert FROM Clients";
@@ -116,6 +146,7 @@ function getUsersList(){
                     reject(err);
                 else{
                     let usersArray = [];
+                    // convert the lines of the db to usernames array
                     Object.keys(rows).forEach(key => {
                         usersArray.push({username: rows[key].username, cert: rows[key].cert});
                     });
@@ -128,6 +159,13 @@ function getUsersList(){
     return executeQuery(query);
 }
 
+/**
+ * Push some message to server
+ * @param from - the username that the message was sended from
+ * @param to - the username that the message was sended to
+ * @param message
+ * @returns Promise that invoke the query and return if succeeded
+ */
 function pushMessageToServer(from,to,message){
     let query = (db) => {
         let push = `INSERT INTO Messages ("to","from",message) VALUES ('${to}','${from}','${message}')`;
@@ -148,24 +186,29 @@ function pushMessageToServer(from,to,message){
     return executeQuery(query);
 }
 
+/**
+ * Gets all the waiting messages for specific user from the db and delete them afterwords
+ * @param toUser - the user that the messages are intended to
+ * @returns Promise that gets the messages from the db
+ */
 function getWaitingMessages(toUser){
     let query = (db) => {
         let getMessages = `SELECT "from",message FROM Messages WHERE "to"='${toUser}'`;
         let deleteMessages = `DELETE FROM Messages WHERE "to"='${toUser}'`;
 
         return new Promise((resolve,reject) => {
-            db.all(getMessages, function(err, rows){
+            db.all(getMessages, function(err, rows){ // get all the messages with the username in the "to" column
                 if (err) {
                     closeDb(db);
                     reject(err);
                 }
                 else{
                     let messages = [];
-                    Object.keys(rows).forEach(key => {
+                    Object.keys(rows).forEach(key => { // convert the lines of the db to messages array
                         messages.push({from: rows[key].from, message: rows[key].message});
                     });
 
-                    db.all(deleteMessages, function(err, rows) {
+                    db.all(deleteMessages, function(err, rows) {    // delete all that messages from the db
                         closeDb(db);
                         if (err)
                             reject(err);
@@ -180,6 +223,11 @@ function getWaitingMessages(toUser){
     return executeQuery(query);
 }
 
+/**
+ * Get specific user certificates from the server
+ * @param username - the wanted user
+ * @returns Promise that gets the cert from the db
+ */
 function getUserCert(username){
     let query = (db) => {
         let getCert = `SELECT cert FROM Clients WHERE username='${username}'`;
@@ -199,21 +247,28 @@ function getUserCert(username){
     return executeQuery(query);
 }
 
+/**
+ * Close connection to db
+ * @param db - the connection
+ */
 function closeDb(db) {
     db.close(function (err) {
         if (err) {
             console.log(err.message);
         }
-        //console.log('Close the database connection.');
     });
 }
 
+/**
+ * Gets query, creates a db instance and invoke the query with the db instance as a param
+ * @param func - the query to invoke
+ * @returns the result of the query activation
+ */
 function executeQuery(func){
     let db = new sqlite3.Database('./db/server.db', sqlite3.OPEN_READWRITE, function (err) {
         if (err) {
             console.log(err.message);
         }
-        //console.log('Connected to the database.');
     });
 
     return func(db);
